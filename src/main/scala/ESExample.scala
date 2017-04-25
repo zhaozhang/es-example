23fd1543a91da428f8b6ddecb78537e27611572d
zhaozhang <zhangzhao0718@gmail.com>
2017-04-25 11:11:51 -0500

    adpating to tar

--- src/main/scala/ESExample.scala
+++ src/main/scala/ESExample.scala
@@ -1,10 +1,16 @@
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream
+import org.apache.spark.input.PortableDataStream
+import scala.util.Try
+import java.nio.charset._
+
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 
 import org.elasticsearch.spark._ 
 
-case class Item(dt: Long, server: String, content: String)
+case class Item(content: String)
 
 object ESExample {
   def main(args: Array[String]) {
@@ -13,13 +19,41 @@ object ESExample {
 
     val sc = new SparkContext(conf)
 
-    val lines = sc.textFile("nasa_19950801.csv")
+    val src = args(0)
 
-    val r = lines.map( line => {
-      val words = line.split(",")
-      Item(words(2).trim.toLong, words(0).trim.toString, words(4).trim.toString)
-    })
+    val t = sc.binaryFiles(src).flatMap(x => extractFiles(x._2).toOption).flatMap(_.map(decode()))
+    //val tt = t.coalesce(2, shuffle=true)
 
-    r.saveToEs("nasa_complete/19950801")
+    //val files = sc.wholeTextFiles("health-websites/", 32)
+
+    val r = t.map(f => Item(f.trim.toString))
+
+    r.saveToEs("480-health-websites/480")
   }
+
+  def extractFiles(ps: PortableDataStream, n: Int = 1024) = Try {
+    val tar = new TarArchiveInputStream(ps.open)
+    Stream.continually(Option(tar.getNextTarEntry))
+      // Read until next exntry is null
+      .takeWhile(_.isDefined)
+      // flatten
+      .flatMap(x => x)
+      // Drop directories
+      .filter(!_.isDirectory)
+      .map(e => {
+        Stream.continually {
+          // Read n bytes
+          val buffer = Array.fill[Byte](n)(-1)
+          val i = tar.read(buffer, 0, n)
+          (i, buffer.take(i))}
+        // Take as long as we've read something
+        .takeWhile(_._1 > 0)
+        .map(_._2)
+        .flatten
+        .toArray})
+      .toArray
+  }
+
+  def decode(charset: Charset = StandardCharsets.UTF_8)(bytes: Array[Byte]) = 
+    new String(bytes, StandardCharsets.UTF_8)
 }
